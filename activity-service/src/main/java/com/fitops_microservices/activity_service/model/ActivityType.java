1462a8f6b3cb74d87d480bd605600bd03b9fde2a
Sushantpaudel77 <sushantpaudel77@gmail.com>
2025-09-01 01:24:09 +0545

    feat: add activity-service

---
+++ activity-service/src/main/java/com/fitops_microservices/activity_service/model/ActivityType.java
@@ -0,0 +1,13 @@
+package com.fitops_microservices.activity_service.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
