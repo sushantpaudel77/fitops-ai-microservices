8b2ec51d54079ec06d65aea8d086669561e45d71
Sushantpaudel77 <sushantpaudel77@gmail.com>
2025-09-04 22:06:35 +0545

    Feat: add ai-service/model

---
+++ ai-service/src/main/java/fitops_microservices/ai_service/model/Recommendation.java
@@ -0,0 +1,28 @@
+package fitops_microservices.ai_service.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "recommendations")
+@Data
+@Builder
+public class Recommendation {
+
+    @Id
+    private String id;
+    private String activityId;
+    private String userId;
+    private String recommendation;
+    private List<String> improvements;
+    private List<String> suggestions;
+    private List<String> safety;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
