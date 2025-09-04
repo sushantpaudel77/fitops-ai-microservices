91c9751dfb7a1ee2987339f709661862a9a68f25
Sushantpaudel77 <sushantpaudel77@gmail.com>
2025-09-04 22:18:27 +0545

    Feat: add ai-service/controller

---
+++ ai-service/src/main/java/fitops_microservices/ai_service/controller/RecommendationController.java
@@ -0,0 +1,31 @@
+package fitops_microservices.ai_service.controller;
+
+import fitops_microservices.ai_service.model.Recommendation;
+import fitops_microservices.ai_service.service.RecommendationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/recommendations")
+public class RecommendationController {
+
+    private final RecommendationService recommendationService;
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Recommendation>> getUserRecommendation(@PathVariable String userId) {
+        return ResponseEntity.ok(recommendationService.getUserRecommendation(userId));
+    }
+
+
+    @GetMapping("/activity/{activityId}")
+   public ResponseEntity<Recommendation> getActivityRecommendation(@PathVariable String activityId) {
+        return ResponseEntity.ok(recommendationService.getActivityRecommendation(activityId));
+    }
+}
---
+++ ai-service/src/main/java/fitops_microservices/ai_service/service/RecommendationService.java
@@ -0,0 +1,4 @@
+package fitops_microservices.ai_service.service;
+
+public class RecommendationService {
+}
